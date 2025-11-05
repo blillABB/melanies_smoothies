34c8e0cb82152abee0de1440af9f82140519eb5b
blillABB <blake.lillard@us.abb.com>
2025-11-05 10:06:42 -0600

    Update streamlit_app.py
    added a dataframe to display the response JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,5 @@ if ingredients_list:
 # new section to display smoothiefroot nutrition info
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_reponse.json(), use_container_width=True)
